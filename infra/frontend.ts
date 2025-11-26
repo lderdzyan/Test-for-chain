e909f818adb98928048d7bd8c6a1c7bdb0bfe25d
lderdzyan <lderdzyan@vtgsoftware.com>
2025-11-26 15:26:18 +0400

    this is test for new ci

--- infra/frontend.ts
+++ infra/frontend.ts
@@ -25,7 +25,7 @@ export class FrontendStack extends TerraformStack {
         new AwsProvider(this,"aws",{});
 
 	const myBucket = new  S3Bucket(this,"myBucket",{
-		bucket: "mybacketuniqetestthisistest"
+		bucket: "mybacketuniqetestthisistesterkarache"
 	});
 	
 	const myOac = new CloudfrontOriginAccessControl(this,"myOac",{
