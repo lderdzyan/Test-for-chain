add6d9e07cf0ea8c544a4d02c6cadabe93adadbc
lderdzyan <lderdzyan@vtgsoftware.com>
2025-12-02 16:04:37 +0400

    workflow test

--- infra/frontend.ts
+++ infra/frontend.ts
@@ -37,7 +37,7 @@ export class FrontendStack extends TerraformStack {
 	});
 	
 	const myOac = new CloudfrontOriginAccessControl(this,"myOac",{
-		name: `myOacfortestwillchange`,
+		name: `myOacforpoject`,
 		originAccessControlOriginType: "s3",
 	        signingBehavior: "always",
       		signingProtocol: "sigv4",
