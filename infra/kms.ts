f7fc472344fbcb9b6f4e8a2f71a23b5803f7f7a3
lderdzyan <lderdzyan@vtgsoftware.com>
2025-12-04 15:19:40 +0400

    kms push

--- infra/kms.ts
+++ infra/kms.ts
@@ -2,16 +2,20 @@ import { Construct } from "constructs";
 import { KmsKey } from "./.gen/providers/aws/kms-key";
 import { DataAwsCallerIdentity } from "./.gen/providers/aws/data-aws-caller-identity";
 
-export class KmsStack extends Construct {
+export class KmsStack extends TerraformStack {
   public readonly kmsKey: KmsKey;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    new AwsProvider(this, "aws_kms", {
+      region: "eu-central-1"
+    });
+
     const account = new DataAwsCallerIdentity(this, "account");
 
     this.kmsKey = new KmsKey(this, "projectKmsKey", {
-      description: "Master encryption key for all project resources",
+      description: "Master KMS key",
       enableKeyRotation: true,
       deletionWindowInDays: 7,
       policy: JSON.stringify({
